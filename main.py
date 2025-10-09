b33aa0691603a1f6bf4307132e6b2cdbab2fc931
ro011110ot <ro011110ot@gmail.com>
2025-10-09 15:40:56 +0200

    add ssd1306_driver.py and oled.py to Display "Hello World"

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 # Import own Functions
 import dht11
+import oled
 import wifi
 
 # Imports
 # from time import sleep    # activate when measure every 30 Min !
 
 wifi.connect()
-
+oled.oled_hello()
 dht11.dht11()
 
 # measure every 30 Min
