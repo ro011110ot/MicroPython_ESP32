8005b5030a6b1e2bef212898bc38cca60525898b
ro011110ot <ro011110ot@gmail.com>
2025-10-09 20:57:45 +0200

    add import from secrets import secrets

--- wifi.py
+++ wifi.py
@@ -1,30 +1,28 @@
-import network
-import time
-import ntp
 import machine
+import network
+import ntp
 import ntptime
+import time
+from secrets import secrets
 
 # WLAN-Konfiguration
 wlan = network.WLAN(network.WLAN.IF_STA)
-wlanSSID = 'ro011110ot'
-wlanPW = 'interNET'
-network.country('DE')
 
 
 # Funktion: WLAN-Verbindung
 def connect():
     wlan.active(True)
     if not wlan.isconnected():
-        print('connecting to network...')
-        wlan.connect(wlanSSID, wlanPW)
+        print("connecting to network...")
+        wlan.connect(secrets["ssid"], secrets["password"])
         while not wlan.isconnected():
             time.sleep(1)
 
     if wlan.isconnected():
-        print('network config:', wlan.ipconfig('addr4'))
+        print("network config:", wlan.ipconfig("addr4"))
         ntptime.settime()
         rtc = machine.RTC()
         rtc.datetime(ntp.cettime())
         print("RTC set to:", time.localtime())
     else:
-        print('Network ro011110ot is offline')
+        print("Network ro011110ot is offline")
