2776e6b972e4418ec84d3bac3821e56e3e1481fe
ro011110ot <ro011110ot@gmail.com>
2025-10-09 14:44:26 +0200

    Remove print from dht11.py and write the data to temp.csv

--- dht11.py
+++ dht11.py
@@ -1,23 +1,35 @@
 import dht
 from machine import Pin
-from time import sleep
+import time
+import os
 
 sensor = dht.DHT11(Pin(14))
 
-
-# noinspection PyUnusedLocal
 def dht11():
-    while True:
+    # Check if temp.csv exists, if not create it with header
+    try:
+        os.stat('temp.csv')
+    except OSError:
+        with open('temp.csv', 'w') as f:
+            f.write('date;time;temperature;humidity\n')
+
+    try:
+        sensor.measure()
+        temp = sensor.temperature()
+        hum = sensor.humidity()
+
+        # Get current time
+        now = time.localtime()
+        date_str = "{:04d}.{:02d}.{:02d}".format(now[0], now[1], now[2])
+        time_str = "{:02d}:{:02d}".format(now[3], now[4])
+
+        # Format data for CSV
+        csv_line = f"{date_str};{time_str};{temp} °C;{hum} %\n"
+        # Write to CSV
         try:
-
-            sensor.measure()
-            temp = sensor.temperature()
-            hum = sensor.humidity()
-            # temp_f = temp * (9 / 5) + 32.0
-            # print('Temperature: %3.1f F' % temp_f)
-            print('Temperature: %3.1f C' % temp)
-            print('Humidity: %3.1f %%' % hum)
-
-            sleep(30)
+            with open('./temp.csv', 'a') as f:
+                f.write(csv_line)
         except OSError as e:
-            print('Failed to read sensor.')
+            print('Failed to write to temp.csv')
+    except OSError as e:
+        print('Failed to read sensor.')
--- main.py
+++ main.py
@@ -1,5 +1,15 @@
-import wifi
+# Import own Functions
 import dht11
+import wifi
+
+# Imports
+# from time import sleep    # activate when measure every 30 Min !
+
 wifi.connect()
 
 dht11.dht11()
+
+# measure every 30 Min
+# while True:
+    # dht11.dht11()
+    # sleep(1800)
