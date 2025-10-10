1ab80578f4f4c192a54c372ab4a1f690ba9bb27f
ro011110ot <ro011110ot@gmail.com>
2025-10-10 13:51:32 +0200

    added while loop to call localtime und measure dht11 every 15 Min

--- main.py
+++ main.py
@@ -4,8 +4,7 @@ import oled
 import wifi
 import time
 
-time_now = time.localtime()
-min_now = time_now[4]
+
 # Imports
 from time import sleep, localtime  # activate when measure every 30 Min !
 
@@ -13,5 +12,9 @@ wifi.connect()
 oled.start_timer()
 
 # measure every 15 Min
-if min_now == 00 or min_now == 15 or min_now == 30 or min_now == 45:
-    dht11.dht11()
+while True:
+    time_now = time.localtime()
+    min_now = time_now[4]
+    if min_now == 00 or min_now == 15 or min_now == 30 or min_now == 45:
+        dht11.dht11()
+    time.sleep(60)
